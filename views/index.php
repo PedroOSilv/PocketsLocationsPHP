1fee6dfa0fee2ca7c3ca26ac4bbf4f46453b174a
PedroOSilv <81320895+PedroOSilv@users.noreply.github.com>
2024-10-25 11:00:40 -0400

    update iconAnchor leaflet

--- views/index.php
+++ views/index.php
@@ -60,7 +60,7 @@
         shadowUrl: '',
         iconSize: [32, 32],
         shadowSize: [50, 64],
-        iconAnchor: [22, 94],
+        iconAnchor: [16, 32],
         shadowAnchor: [4, 62],
         popupAnchor: [-3, -76]
     });
